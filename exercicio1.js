908421a31c8399bd4eb5a5f9b249e239e09d11e3
Your Name <you@example.com>
2018-04-19 11:59:29 -0300

    final da aula

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,9 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+const btnExpand = document.querySelectorAll('.botao-expandir-retrair');
+btnExpand.forEach(function(el) {
+  el.addEventListener('click', function(evento) {
+    	el.parentNode.classList.toggle('expandido');
+  });
+});
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,22 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+var at=0;
+
+document.getElementById("proximo").addEventListener('click', function(evento) {
+	at++;
+	if(at>4){
+		at=0;
+	}
+  	document.getElementById("slide").src = servidorDasImagens+todasAsImagens[at];
+	console.log(at);
+ });
+
+document.getElementById("anterior").addEventListener('click', function(evento) {
+	at--;
+	if(at<0){
+		at=4;
+	}
+  	document.getElementById("slide").src = servidorDasImagens+todasAsImagens[at];
+	console.log(at);
+});
